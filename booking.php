1cc69b68448f41b8b2a6058fcb505354a6cbc20b
berenypanda <100977730+berenypanda@users.noreply.github.com>
2023-04-12 12:51:01 -0500

    Create booking.php

---
+++ booking.php
@@ -0,0 +1,17 @@
+<br> 
+<form action= "post.php" method="post">
+Enter the following information:
+    Name* : <input type="text" name="name"><br/>
+    <br/>  
+    Email*: <input type="text" name="email"><br/>
+    <br/>
+    Service*:
+    <select name="service">
+    <option value="Color/Highlights">Color/Highlights</option>
+    <option value="Hair Cut">Hair Cut</option>
+    <option value="Styling">Styling</option></select>
+    <br/>
+    <br/>
+    Comment: <br/><textarea name="comment" rows="5" cols="40"></textarea><br>
+    <input type="Submit" name="SubmitThis" value="Preview">
+</form>
