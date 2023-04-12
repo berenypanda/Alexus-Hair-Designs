e9717ce5f478dc19cd8deb2577636dc255b75b15
berenypanda <100977730+berenypanda@users.noreply.github.com>
2023-04-12 13:08:41 -0500

    Create shared.php for reusuable elements

---
+++ shared.php
@@ -0,0 +1,29 @@
+<?php
+
+
+$logo = "<header>
+<img class='logo' src='placeholder.png' alt='logo'>
+<span><a href='index.html'></a></span>
+</header>";
+	
+$nav = "<nav>
+    <ul class='navigation collapse' id='navigation'>
+        <li><a href='#' title='Home'>Home</a></li>
+        <li><a href='#' title='Services'>Services</a></li>
+        <li><a href='#' title='Portfolio'>Portfolio</a></li>
+        <li><a href='#' title='Contact'>Contact</a></li>
+        <li><a href='#' title='About'>About</a></li>
+    </ul>
+    <button class='navbar-toggler' type='button' data-target='#navigation'>
+        <span class='fa-solid fa-bars'></span>
+    </button>
+</nav>";
+
+$footer = "<footer>
+<div class='social-media'></div>
+<p>Alexus Hair Designs &copy;2023</p>
+</footer>";
+
+
+
+?>
