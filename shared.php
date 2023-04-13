456a76bc97d87a205a0a39f67b6557501693c121
berenypanda <100977730+berenypanda@users.noreply.github.com>
2023-04-12 19:55:45 -0500

    Update shared.php

--- shared.php
+++ shared.php
@@ -5,19 +5,18 @@
 	
 $navbar = 
 "<div class='navbar'>
-    <header>
-    <img class='logo' src='logo.png' alt='logo'>
-    <span><a href='index.html'></a></span>
+    <header class='col-xs-6 col-lg-4'>
+    <a href='index.php'><img class='logo' src='logo.png' alt='logo'></a>
     </header>
-    <nav>
+    <nav class='col-xs-3 col-lg-7'>
     <ul class='navigation collapse' id='navigation'>
-        <li><a href='#' title='Home'>Home</a></li>
+        <li><a href='index.php' title='Home'>Home</a></li>
         <li><a href='#' title='Services'>Services</a></li>
         <li><a href='#' title='Portfolio'>Portfolio</a></li>
         <li><a href='#' title='Contact'>Contact</a></li>
         <li><a href='#' title='About'>About</a></li>
     </ul>
-    <button class='navbar-toggler' type='button' data-target='#navigation'>
+    <button class='navbar-toggler col-xs-3' type='button' data-target='#navigation'>
         <span class='fa-solid fa-bars'></span>
     </button>
     </nav>
