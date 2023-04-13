f0fa0827b6ecbb28214d69bc19bd2d7fb0cdd824
berenypanda <100977730+berenypanda@users.noreply.github.com>
2023-04-12 21:28:31 -0500

    Add post.php file

---
+++ post.php
@@ -0,0 +1,72 @@
+<?php
+if (array_key_exists("SubmitThis", $_POST)) {
+    $required = array('name', 'email','service');
+	$expected = array('name', 'email','service','comment');
+	$missing = array();
+
+	foreach($expected as $thisField) {
+
+        if (is_array($_POST[$thisField])) {
+            $thisUserInput = $_POST[$thisField];
+        } else {
+            $thisUserInput = trim($_POST[$thisField]);
+        }
+
+		if (in_array($thisField, $required)) {
+
+			if (empty($thisUserInput)) {
+				array_push($missing, $thisField);
+			} else {
+
+				${$thisField} = $thisUserInput;
+			}
+		} else {
+			${$thisField} = $thisUserInput;
+		}
+	
+	}
+	if (empty($missing)){
+
+		$service = implode(", ", $service); //stuck here
+
+
+		if (!empty($email)) {
+			$name = "<a href='mailto:$email'>$name</a>";
+		}
+
+        $output = "<table class = $class border = '1'>\n>
+				<tr>
+					<th>Name:</th>
+					<td> $name</td>
+				</tr>\n 
+				
+				<tr>
+					<th>Email:</th>
+					<td> $email</td>
+				</tr>\n 
+				
+				<tr>
+					<th>Service:</th>
+					<td> $service</td>
+				</tr>\n 
+				<tr>
+					<th>Comment:</th>
+					<td>$comment</td>
+				</tr>\n 
+				
+                </table>\n";
+	
+	} else {
+		$missingFieldList = implode(", ",$missing);
+		$output = "The following required fields are missing. Please go back and fill in the required fields.  Thank you. <br>
+						<b>Missing fields: $missingFieldList </b>
+					";
+
+	}
+    
+} else {
+	$output = "Please post your message use <a href='index.php'>this form</a>.";
+}
+
+
+?>
